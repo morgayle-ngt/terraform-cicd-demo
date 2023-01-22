712d5e2eb6185a036d5c062343696a0d1ac671ab
“morgayle-ngt” <morgayle@nextgentek.io>
2023-01-21 22:16:02 -0500

    Final version of Automate EC2 Part 2

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -88,6 +89,15 @@ output "aws_ami_id" {
     value = data.aws_ami.latest-amazon-linux-image.id
 }
 
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+}
+
 resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
@@ -97,7 +107,7 @@ resource "aws_instance" "myapp-server" {
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
-    key_name = "server-key-pair"
+    key_name = aws_key_pair.ssh-key.key_name
 
     tags = {
         Name = "${var.env_prefix}-server"
